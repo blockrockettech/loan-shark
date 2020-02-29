fe1880b3252317cb85dd23615fc93767fb55ffb5
atkinsonholly <atkinsonholly@gmail.com>
2020-02-29 15:00:28 +0000

    WIP redux setup

---
+++ client/src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers/rootReducer'
+export default function configureStore() {
+ return createStore(
+  rootReducer, applyMiddleware(thunk)
+ )
+}
